f830751e3b2d18233ec4f93b449f466c3f42eb78
Austin Burgard <awburgard@gmail.com>
2019-05-28 13:18:41 -0500

    adds get route to zoo router

--- server/routes/zoo.router.js
+++ server/routes/zoo.router.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 router.get('/', (req, res) => {
-    // YOUR CODE HERE
-    
+    const queryString = `SELECT * FROM "species";`;
+    pool.query(queryString)
+    .then((response) => {
+        res.send(response.rows);
+    })
+    .catch((err) => {
+        res.sendStatus(500);
+    })
 });
 
 module.exports = router;
